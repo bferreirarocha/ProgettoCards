6f5791d32bde493cc156c5ea55658d7dfdeec88b
bferreirarocha <bruno.ferreira.rocha@gmail.com>
2022-01-17 21:27:29 +0100

    fix

--- src/components/Main/Section/MessageDetail/index.jsx
+++ src/components/Main/Section/MessageDetail/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { iconsList } from "../../../../services/iconsList";
 import "./style.scss";
+
 const MessageDetail = (props) => {
-  return <div className="MessageDetail">Ciao</div>;
+  return (
+    <div className="MessageDetail">
+      <div className="header">
+        <div className="avatar">{iconsList[0][props.avatar]}</div>
+        <div className="from"></div>
+      </div>
+      <div className="bodyMsg"></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MessageDetail;
